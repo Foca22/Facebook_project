27ce2ec7d39fd0642566a36dbb8723e431a814fe
Andrei Opris <andrei.oprisss@gmail.com>
2022-08-09 15:07:07 +0300

    Added user and post entities

---
+++ src/main/java/com/project/facebook/model/post/Post.java
@@ -0,0 +1,55 @@
+package com.project.facebook.model.post;
+
+import com.project.facebook.model.users.Users;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users users;
+
+    public Post() {
+    }
+
+    public Post(Integer id, String text, Users users) {
+        this.id = id;
+        this.text = text;
+        this.users = users;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Users getUser() {
+        return users;
+    }
+
+    public void setUser(Users users) {
+        this.users = users;
+    }
+}
